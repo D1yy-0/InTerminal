d2434ae24c23b8ebe96c6ec4a760fb5a4e3582ff
Diyar Mohammed <diyarmohammed097@gmail.com>
2026-06-28 01:25:58 +0300

    Implement initial game engine

---
+++ src/game.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "game.h"
+
+void game_run(void) {
+
+    printf("Game is Starting...\n");
+
+    printf("Initializing game...\n");
+
+    printf("Entering game loop...\n");
+
+    printf("Shutting down...\n");
+
+    printf("Goodbye!\n");
+
+    scanf("%*c"); // Wait for user input before exiting
+
+}
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "game.h"
+
+
+int main(void){
+
+    game_run();
+
+    return 0;
+}
