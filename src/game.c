68cdd370b042245e4a45f0ce679c5b6d5ef328a5
Diyar Mohammed <diyarmohammed097@gmail.com>
2026-06-28 02:11:32 +0300

    Implement basic game loop architecture

--- src/game.c
+++ src/game.c
@@ -1,18 +1,71 @@
 #include <stdio.h>
 #include "game.h"
 
+
+/* Private Functions prototypes */
+
+static void game_initialize(void);
+static void game_input(void);
+static void game_update(void);
+static void game_render(void);
+static void game_shutdown(void);
+
+/* Private State */
+
+static int running = 0;
+
+/* Public Functions */
+
 void game_run(void) {
 
-    printf("Game is Starting...\n");
+    game_initialize();
 
-    printf("Initializing game...\n");
+    while (running) {
 
-    printf("Entering game loop...\n");
+        game_input();
 
-    printf("Shutting down...\n");
+        game_update();
 
-    printf("Goodbye!\n");
+        game_render();
 
-    scanf("%*c"); // Wait for user input before exiting
+        /* if ( // some condition to exit the game ) {
+            running = 0;
+        } */
+    }
+
+    game_shutdown();
 
 }
+
+static void game_initialize(void) {
+
+    running = 1;
+
+    printf("Game initializing...\n");
+
+}
+
+static void game_input(void) {
+
+    printf("Game input processing...\n");
+
+}
+
+static void game_update(void) {
+
+    printf("Game updating...\n");
+
+}
+
+static void game_render(void) {
+
+    printf("Game rendering...\n");
+
+}
+
+static void game_shutdown(void) {
+
+    printf("Game shutting down...\n");
+
+}
+
